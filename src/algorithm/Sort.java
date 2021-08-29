5166e07e6068b41f1d982b548cef0b23f66ea209
= <=>
2021-08-29 18:09:17 -0500

    working on sorting algo

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import java.util.Arrays;
+
 public class Sort {
 
     long executionTime = 0;
@@ -9,10 +11,9 @@ public class Sort {
      */
 
     public static void printSortedArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        System.out.print(Arrays.toString(array));
         }
-    }
+
 
     public int[] selectionSort(int[] array) {
         final long startTime = System.currentTimeMillis();
@@ -39,7 +40,16 @@ public class Sort {
     public int[] insertionSort(int[] array) {
         final long startTime = System.currentTimeMillis();
         int[] list = array;
-        //implement here
+        int temp;
+        for (int i = 1; i < array.length; i++) {
+            int j = i;
+            while ((j > 0) && (array[j] < array[j-1])) {
+                temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+                j--;
+            }
+        }
 
         final long endTime = System.currentTimeMillis();
         final long executionTime = endTime - startTime;
@@ -48,16 +58,37 @@ public class Sort {
     }
 
     public int[] bubbleSort(int[] array) {
+        final long startTime = System.currentTimeMillis();
         int[] list = array;
-        //implement here
+        int temp;
 
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 1; j < list.length - i; j++) {
+                if (list[j-1] > list[j]) {
+                    temp = list[j-1];
+                    list[j-1] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        final long endTime = System.currentTimeMillis();
+        final long executionTime = endTime - startTime;
+        this.executionTime = executionTime;
         return list;
     }
 
-    public int[] mergeSort(int[] array) {
+    public int[] mergeSort(int[] array, int l, int r) {
+        final long startTime = System.currentTimeMillis();
         int[] list = array;
-        //implement here
-
+        if (l < r) {
+            int m = l + (r-l)/2;
+            mergeSort(list, l, m);
+            mergeSort(list, m+1, r);
+            merge(list, l, m, r);
+        }
+        final long endTime = System.currentTimeMillis();
+        final long executionTime = endTime - startTime;
+        this.executionTime = executionTime;
         return list;
     }
 
@@ -87,5 +118,36 @@ public class Sort {
         //implement here
 
         return list;
+    }
+
+
+
+        //HELPER METHOD FOR MERGESORT:
+    public static void merge(int[] array, int l, int m, int r) {
+        int n1 = m-l+1;
+        int n2 = r-m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for(int i = 0;i < n1; i++) {
+            L[i] = array[l+i];
+        }
+        for(int i = 0;i < n2; i++) {
+            R[i] = array[m+1+i];
+        }
+        int i = 0, j = 0, k =l;
+        while(i < n1 && j < n2) {
+            if(L[i] <= R[j]) {
+                array[k++] = L[i++];
+            }
+            else {
+                array[k++] = R[j++];
+            }
+        }
+        while(i < n1) {
+            array[k++] = L[i++];
+        }
+        while(j < n2) {
+            array[k++] = R[j++];
+        }
     }
 }
--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -2,29 +2,46 @@ package algorithm;
 
 import org.testng.Assert;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class UnitTestSorting {
+
+
 
     /*
       This class is used to help with unit testing of sorting algorithms from the Sort class
      */
     public static void main(String[] args) {
-        int[] unSortedArray = {6, 9, 2, 5, 1, 0, 4};
-        int[] sortedArray = {0, 1, 2, 4, 5, 6, 9};
+        Sort sortArgo = new Sort();
 
-        // Create instance of Sort class
-        Sort sort = new Sort();
+        int[] arr = {10,15,16,12,11,20,19,13,14,18,17};
+        sortArgo.mergeSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
 
-        // Pass the unsorted array to selectionSort() method from Sort class
-        sort.selectionSort(unSortedArray);
 
-        // Verify if the unsorted array is sorted by the selectionSort() method
-        try {
-            Assert.assertEquals(sortedArray, unSortedArray, "ARRAY IS NOT SORTED... YET!\n***YOU GOT THIS!***");
-        } catch (Exception ex) {
-            ex.getMessage();
-        }
+
+
+
+
+//        int[] unSortedArray = {6, 9, 2, 5, 1, 0, 4};
+//        int[] sortedArray = {0, 1, 2, 4, 5, 6, 9};
+//
+//        // Create instance of Sort class
+//        Sort sort = new Sort();
+//
+//        // Pass the unsorted array to selectionSort() method from Sort class
+//        sort.selectionSort(unSortedArray);
+//
+//        // Verify if the unsorted array is sorted by the selectionSort() method
+//        try {
+//            Assert.assertEquals(sortedArray, unSortedArray, "ARRAY IS NOT SORTED... YET!\n***YOU GOT THIS!***");
+//        } catch (Exception ex) {
+//            ex.getMessage();
+//        }
 
         // Implement Unit test for rest of the sorting algorithms below
 
     }
-}
+
