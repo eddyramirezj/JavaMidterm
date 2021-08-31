5f7a622467b6709a2a801289bdae036b770fd095
= <=>
2021-08-30 21:43:31 -0500

    math.problems.LowestNumber solved

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -14,6 +14,24 @@ public class LowestNumber {
 
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
 
+        System.out.println(findLowestNumber(array));
+
+    }
+
+    static public int findLowestNumber(int[] array) {
+        int[] list = array;
+        int temp;
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 1; j < list.length - i; j++) {
+                if (list[j-1] > list[j]) {
+                    temp = list[j-1];
+                    list[j-1] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        return list[0];
 
     }
 
