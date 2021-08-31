3ccc057c0cfc1f2ae614ad9babc92e7cb3a5ddb9
= <=>
2021-08-30 22:34:45 -0500

    solved math.problems. Pattern problem still pending

--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -12,4 +12,10 @@ public class Pattern {
         */
 
     }
+
+    public static void findPattern(){
+
+
+    }
+
 }
--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -1,5 +1,8 @@
 package math.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeNumber {
 
     public static void main(String[] args) {
@@ -11,6 +14,41 @@ public class PrimeNumber {
          Print out the prime numbers in the given range.
          */
 
+        System.out.println(primeNumbers(1000000));
+
     }
 
-}
+        public static List<Integer> primeNumbers(int maxNumber) {
+        List<Integer> primeNumbers = new ArrayList<>();
+
+            if (maxNumber >= 2) {
+                primeNumbers.add(2);
+            }
+            for (int i = 3; i <= maxNumber; i += 2) {
+                if (isPrime(i)) {
+                    primeNumbers.add(i);
+                }
+            }
+            return primeNumbers;
+        }
+
+        //HELPER METHOD:
+
+        private static boolean isPrime(int number) {
+            for (int i = 2; i*i <= number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+
+            }
+
+
+        }
+
+
+
+
+
+
