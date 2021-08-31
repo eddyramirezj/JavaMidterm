9a93a093e7d797db5a0556dd801e97f3b8440fe3
= <=>
2021-08-30 20:20:02 -0500

    working on math.problems. Fibonacci completed

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -10,8 +10,45 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
+        fibonacciNumbers(40);
+
+    }
+
+    public static void fibonacciNumbers(int number) {
+        int a = 0;
+        int b = 1;
+        int c;
+
+        System.out.print(a + ",");
+        System.out.print(b + ",");
+        c = a + b;
+        System.out.print(c + ",");
+        a = c;
+        c = a + b;
+        System.out.print(c + ",");
+        a = c;
+        b += a;
+        System.out.print(b + ",");
+        for (int x = 0; x <= number; x++) {
+            c = a + b;
+            System.out.print(c + ",");
+            a = c;
+
+            c = a + b;
+            System.out.print(c + ",");
+            b = c;
+
+            c = a + b;
+            System.out.print(c + ",");
+            a = c;
+
+            c = a + b;
+            System.out.print(c + ",");
+            b = c;
+            }
+        }
+
 
     }
 
 
-}
